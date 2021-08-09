5286970468af316964cd595ee702445aa8000467
jigglypop <52653682+jigglypop@users.noreply.github.com>
2021-08-09 15:31:55 +0900

    0807

--- Desktop/리액트버전2/react/Closure.js
+++
@@ -1,9 +0,0 @@
-import React from "./react.js";
-// 함수 내부에서 react 훅을 쓸 수 있게 바인딩
-export const Closure = function (cb, $target, _value) {
-  const react = new React();
-  const _cb = cb.bind(react);
-  return [react, _cb, $target];
-};
-
-export default Closure;
