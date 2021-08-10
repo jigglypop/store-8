9a78efd426b3c21cf7693fd5856cdd8971d847b8
sshrik <chj878194@naver.com>
2021-08-10 23:29:21 +0900

    [#11] feat: add Constants for header.

---
+++ client/src/constants/Cart.ts
@@ -0,0 +1,7 @@
+const ORDER_READY: number = 1;
+const ORDER_START: number = 2;
+const ORDER_FINISH: number = 3;
+const PROGRESS_TYPE_GREY: string = 'grey';
+const PROGRESS_TYPE_BLACK: string = 'black';
+
+export { ORDER_READY, ORDER_START, ORDER_FINISH, PROGRESS_TYPE_GREY, PROGRESS_TYPE_BLACK };
