6d8ed172e0d0bbf5efdc6a1e7b4a599de0f62fce
hayoung123 <proshy1@naver.com>
2021-08-10 23:44:26 +0900

    [#9] style: zoomImage 마크업

---
+++ client/src/components/common/ZoomImg.tsx
@@ -0,0 +1,24 @@
+import { ReactElement } from 'react';
+import styled from 'styled-components';
+
+interface Props {
+  src: string;
+}
+
+export default function ZoomImg({ src }: Props): ReactElement {
+  return (
+    <StyledZoomImg>
+      <img src={src} alt="image" />
+    </StyledZoomImg>
+  );
+}
+
+const StyledZoomImg = styled.div`
+  width: 500px;
+
+  padding: 0 20px;
+  & > img {
+    width: 480px;
+    height: 580px;
+  }
+`;
