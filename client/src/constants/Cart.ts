14c32ca67b2e3a1a70cef8095fc7bf194de16449
sshrik <chj878194@naver.com>
2021-08-11 01:11:55 +0900

    [#11] wip: 공통 타입 선언.

--- client/src/constants/Cart.ts
+++ client/src/constants/Cart.ts
@@ -4,4 +4,14 @@ const ORDER_FINISH: number = 3;
 const PROGRESS_TYPE_GREY: string = 'grey';
 const PROGRESS_TYPE_BLACK: string = 'black';
 
+export type ContentData = {
+  isCoupon: boolean;
+  isChecked: boolean;
+  imgLink: string;
+  title: string;
+  option: string;
+  count: number;
+  amount: number /* TODO : 금액을 number 로 할지, Big Int 로 할지? */;
+};
+
 export { ORDER_READY, ORDER_START, ORDER_FINISH, PROGRESS_TYPE_GREY, PROGRESS_TYPE_BLACK };
