38b1cc911e249f00f2af7dc116baaec3854aefd4
sshrik <chj878194@naver.com>
2021-08-10 23:28:28 +0900

    [#11] feat: add Global Style

--- client/src/GlobalStyle.ts
+++ client/src/GlobalStyle.ts
@@ -1,8 +1,18 @@
-import { createGlobalStyle } from "styled-components";
-import { normalize } from "styled-normalize";
+import { createGlobalStyle } from 'styled-components';
+import { normalize } from 'styled-normalize';
 
 export const GlobalStyle = createGlobalStyle`
   ${normalize}
+  body {
+    width: 100vw;
+    min-height: 100vh;
+    --cartGrey: #D1D1D1;
+    --realBlack: #FFFFF;
+    --pastelBlack: #333333;
+  }
+  p, h1, h2, h3, h4, h5, h6, div, span, input, input::placeholder, button {
+    font-family: "NotoSans", "Nanum Gothic", sans-serif;
+  }
 `;
 
 export default GlobalStyle;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import * as S from './GlobalStyle';
 
 const rootEl = document.getElementById('root');
 
 ReactDOM.render(
   <React.StrictMode>
+    <S.GlobalStyle />
     <App />
   </React.StrictMode>,
   rootEl
