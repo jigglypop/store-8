927ad321cef87c7ee1ffde4af3f3553b24ab45e1
sshrik <chj878194@naver.com>
2021-08-10 23:28:48 +0900

    [#11] feat: add Header

--- client/src/components/Cart/Header/index.tsx
+++ client/src/components/Cart/Header/index.tsx
@@ -1,13 +1,31 @@
 import React, { ReactElement } from 'react';
 import * as S from './style';
+import cartNextBlack from '@image/cartNextBlack.png';
+import cartNextGrey from '@image/cartNextGrey.png';
+import { ORDER_FINISH, ORDER_READY, ORDER_START } from '@constants/Cart';
 
-interface Props {}
+interface CartHeaderProps {
+  nowStep: number;
+}
 
-function CartHeader({}: Props): ReactElement {
+function CartHeader({ nowStep }: CartHeaderProps): ReactElement {
+  const getImgSourceStep = (destStep: number) => {
+    return nowStep === destStep ? cartNextBlack : cartNextGrey;
+  };
+  const getTextColorStep = (destStep: number) => {
+    return nowStep === destStep ? 'prgoress' : 'wait';
+  };
   return (
     <S.CartHeader>
-      <div>
-        
+      <div className="header-container">
+        <h2 className="header-title">장바구니</h2>
+        <div className="cart-progress-container">
+          <p className={getTextColorStep(ORDER_READY)}>01 장바구니</p>
+          <img src={getImgSourceStep(ORDER_READY)} />
+          <p className={getTextColorStep(ORDER_START)}>02 주문서작성/결제</p>
+          <img src={getImgSourceStep(ORDER_START)} />
+          <p className={getTextColorStep(ORDER_FINISH)}>03 주문완료</p>
+        </div>
       </div>
     </S.CartHeader>
   );
--- client/src/components/Cart/Header/style.tsx
+++ client/src/components/Cart/Header/style.tsx
@@ -1,3 +1,41 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
-export const CartHeader = styled.div``;
+export const CartHeader = styled.div`
+  .header-container {
+    width: calc(100% - 80px);
+    display: flex;
+    justify-content: space-between;
+    padding: 40px;
+  }
+  .header-title {
+  }
+  .cart-progress-container {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+  }
+  .cart-progress-container > p {
+    font-size: 14px;
+    font-weight: bold;
+  }
+  .cart-progress-container > img {
+    height: 10px;
+    width: 10px;
+    margin: 0px 16px 0px 12px;
+  }
+  .progress {
+    color: var(--realBlack);
+  }
+  .wait {
+    color: var(--cartGrey);
+  }
+`;
+
+export const ProgressText = styled.p`
+  .progress {
+    color: var(--realBlack);
+  }
+  .wait {
+    color: var(--cartGrey);
+  }
+`;
