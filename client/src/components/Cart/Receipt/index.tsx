ecb3e88b15ab0af02c3ba92ed341818baacd842e
sshrik <chj878194@naver.com>
2021-08-11 00:37:30 +0900

    [#11] feat: Receipt 객체 추가.

---
+++ client/src/components/Cart/Receipt/index.tsx
@@ -0,0 +1,14 @@
+import React, { ReactElement } from 'react';
+import * as S from './style';
+
+interface Props {}
+
+function Receipt({}: Props): ReactElement {
+  return (
+    <S.Receipt>
+      <div></div>
+    </S.Receipt>
+  );
+}
+
+export default Receipt;
