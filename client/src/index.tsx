d9f3e4f9ab94ff1bf97506600435375d88daed4a
hayoung123 <proshy1@naver.com>
2021-08-11 17:05:10 +0900

    feat: global,reset style

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,16 @@
+import './ResetStyle.tsx';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import GlobalStyle from './GlobalStyle';
+import ResetStyle from './ResetStyle';
 
 const rootEl = document.getElementById('root');
 
 ReactDOM.render(
   <React.StrictMode>
+    <ResetStyle />
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
   rootEl
