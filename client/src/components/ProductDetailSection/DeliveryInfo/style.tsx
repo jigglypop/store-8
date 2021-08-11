a4f6f8c3edc9f3ca72af4926060db10484f22d25
hayoung123 <proshy1@naver.com>
2021-08-12 01:24:15 +0900

    [#9] stlye: 배송안내 마크업

---
+++ client/src/components/ProductDetailSection/DeliveryInfo/style.tsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const DeliveryInfo = styled.div`
+  background-color: var(--white);
+  margin-top: 45px;
+  font-size: var(--body-small-font);
+  color: var(--text-gray);
+
+  .title {
+    color: var(--text-pastel-black);
+    font-size: var(--body-large-font);
+    font-weight: bold;
+    margin-left: 12px;
+    margin-bottom: 16px;
+  }
+
+  .delivery-info__content {
+    display: flex;
+    margin-bottom: 8px;
+  }
+
+  .subtitle {
+    margin-right: 5px;
+  }
+
+  & > div:last-child {
+    margin-top: 32px;
+  }
+`;
