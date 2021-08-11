9096b1d37cf87b0c665c7ffae8ea00e29e6df6c5
hayoung123 <proshy1@naver.com>
2021-08-11 11:28:35 +0900

    style: 글로벌 스타일 추가

--- client/src/GlobalStyle.ts
+++ client/src/GlobalStyle.ts
@@ -1,8 +1,11 @@
-import { createGlobalStyle } from "styled-components";
-import { normalize } from "styled-normalize";
+import { createGlobalStyle } from 'styled-components';
+import { normalize } from 'styled-normalize';
 
 export const GlobalStyle = createGlobalStyle`
   ${normalize}
+  h1,h2,h3{
+    margin:0;
+  }
 `;
 
 export default GlobalStyle;
