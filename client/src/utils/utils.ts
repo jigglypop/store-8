d7453dfa5d48aa8697b5d336a98ad52cae5b0219
sshrik <chj878194@naver.com>
2021-08-11 11:47:27 +0900

    [#11] feat: 배송비 로직

--- client/src/utils/utils.ts
+++ client/src/utils/utils.ts
@@ -1,5 +1,25 @@
+import {
+  SHIPMENT_BASE,
+  SHIPMENT_HIGH,
+  SHIPMENT_HIGH_BASE,
+  SHIPMENT_MIDDLE,
+  SHIPMENT_MIDDLE_BASE,
+  SHIPMENT_LOW,
+  SHIPMENT_LOW_BASE,
+  SHIPMENT_ZERO,
+  SHIPMENT_ZERO_BASE,
+} from '@constants/Cart';
+
 function kstFormatter(amount: number) {
   return amount.toLocaleString() + '원';
 }
 
-export { kstFormatter };
+function getShipmentAmount(amount: number) {
+  if (amount > SHIPMENT_ZERO_BASE) return SHIPMENT_ZERO;
+  else if (amount > SHIPMENT_LOW_BASE) return SHIPMENT_LOW;
+  else if (amount > SHIPMENT_MIDDLE_BASE) return SHIPMENT_MIDDLE;
+  else if (amount > SHIPMENT_HIGH_BASE) return SHIPMENT_HIGH;
+  else return SHIPMENT_BASE;
+}
+
+export { kstFormatter, getShipmentAmount };
