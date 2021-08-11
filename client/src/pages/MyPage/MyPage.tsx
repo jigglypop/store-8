9203bdc7733abe2c0dffdf9b13bfa523db8b664c
0woodev <tc_yeongu.n@woowafriends.com>
2021-08-11 19:47:14 +0900

    [#20] 대소문자로 인한 깃.. 문제발생 해결중 1

---
+++ client/src/pages/MyPage/MyPage.tsx
@@ -0,0 +1,16 @@
+import React, { ReactElement } from 'react';
+import * as S from './style';
+
+import OrderSection from '@src/components/MyPage/OrderSection/OrderSection';
+import NavigationBar from '@src/components/MyPage/NavigationBar/NavigationBar';
+
+interface Props {}
+
+export default function MyPage({}: Props): ReactElement {
+  return (
+    <S.MyPage>
+      <NavigationBar />
+      <OrderSection />
+    </S.MyPage>
+  );
+}
