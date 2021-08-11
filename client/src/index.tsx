26bf92b0019d1d306b4741773783670b85c5e664
hayoung123 <proshy1@naver.com>
2021-08-11 11:29:57 +0900

    [#9] style: 글로벌 스타일 적용

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import GlobalStyle from './GlobalStyle';
 import App from './App';
 
 const rootEl = document.getElementById('root');
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
   rootEl
