5232adf3b7e0309cd031bd67dd4b34207596306d
hayoung123 <proshy1@naver.com>
2021-08-11 15:31:15 +0900

    style: global input,button 초기화

--- client/src/GlobalStyle.ts
+++ client/src/GlobalStyle.ts
@@ -6,6 +6,18 @@ export const GlobalStyle = createGlobalStyle`
   h1,h2,h3{
     margin:0;
   }
+  button,
+  input {
+  outline: none;
+  border: none;
+  background: none;
+  padding: 0;
+  }
+  input[type='number']::-webkit-outer-spin-button,
+  input[type='number']::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
 `;
 
 export default GlobalStyle;
