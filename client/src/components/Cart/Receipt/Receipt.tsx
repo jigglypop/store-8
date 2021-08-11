23ebf4ebbda13440ebd04ac6e26a98d7a1bb7218
sshrik <chj878194@naver.com>
2021-08-11 14:35:05 +0900

    [#11] feat: receipt component 개발.

--- client/src/components/Cart/Receipt/Receipt.tsx
+++ client/src/components/Cart/Receipt/Receipt.tsx
@@ -1,12 +1,58 @@
 import React, { ReactElement } from 'react';
+import { CartContentMetaData } from '@src/types/CartContentMetaData';
+import PlusCircle from '@image/plusCircle.png';
+import EqualCircle from '@image/eqaulCirlce.png';
+import { kstFormatter } from '@src/utils/utils';
 import * as S from './style';
 
-interface Props {}
+interface MetaData {
+  metaData: CartContentMetaData;
+}
 
-function Receipt({}: Props): ReactElement {
+function Receipt({ metaData }: MetaData): ReactElement {
   return (
     <S.Receipt>
-      <div></div>
+      <div className="cart-receipt-container">
+        <div>
+          <div className="cart-receipt-title-layout cart-select-amount-title">
+            <p>{'총'}</p>
+            <p className="cart-title-text-black-large">
+              {kstFormatter(metaData.checkedCount, false)}
+            </p>
+            <p>{'개의 상품금액'}</p>
+          </div>
+          <div className="cart-price-layout">
+            <p className="cart-price-text-black-large">
+              {kstFormatter(metaData.totalPrice, false)}
+            </p>
+            <p className="cart-price-text-unit">{'원'}</p>
+          </div>
+        </div>
+        <img src={PlusCircle} />
+        <div>
+          <div className="cart-receipt-title-layout">
+            <p>{`배송비`}</p>
+          </div>
+          <div className="cart-price-layout">
+            <p className="cart-price-text-black-large">
+              {kstFormatter(metaData.shipmentPrice, false)}
+            </p>
+            <p className="cart-price-text-unit">{'원'}</p>
+          </div>
+        </div>
+        <img src={EqualCircle} />
+        <div>
+          <div className="cart-receipt-title-layout">
+            <p>{`합계`}</p>
+          </div>
+          <div className="cart-price-layout">
+            <p className="cart-price-text-mint-large">
+              {kstFormatter(metaData.totalPrice + metaData.shipmentPrice, false)}
+            </p>
+            <p className="cart-price-text-unit">{'원'}</p>
+          </div>
+        </div>
+      </div>
     </S.Receipt>
   );
 }
--- client/src/components/Cart/Receipt/style.tsx
+++ client/src/components/Cart/Receipt/style.tsx
@@ -1,3 +1,63 @@
 import styled from 'styled-components';
 
-export const Receipt = styled.div``;
+export const Receipt = styled.div`
+  .cart-receipt-container {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    flex-direction: row;
+    width: calc(100% - 150px);
+    border: 1.5px solid var(--cartGrey);
+    margin-top: 40px;
+    padding: 40px;
+  }
+
+  .cart-receipt-container > img {
+    width: 24px;
+    height: 24px;
+    margin-left: 32px;
+    margin-right: 32px;
+  }
+
+  .cart-price-layout {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+  }
+
+  .cart-receipt-title-layout {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+  }
+
+  .cart-select-amount-title {
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+  }
+
+  .cart-title-text-black-large {
+    font-weight: bold;
+    font-size: 16px;
+    margin: 0px 2px;
+  }
+
+  .cart-price-text-black-large {
+    font-weight: bold;
+    font-size: 20px;
+    color: var(--realBlack);
+  }
+  .cart-price-text-mint-large {
+    font-weight: bold;
+    font-size: 20px;
+    color: var(--mint);
+  }
+
+  .cart-price-text-unit {
+    line-height: 20px;
+    font-size: 16px;
+    margin-top: 2px;
+    margin-left: 2px;
+  }
+`;
