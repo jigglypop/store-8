bc23646d3c93710a7a677ed1c6793af32dbf5229
0woodev <tc_yeongu.n@woowafriends.com>
2021-08-13 06:04:19 +0900

    [#20] OrderSection 더미데이터 추가

--- client/components/MyPage/OrderSection/OrderSection.tsx
+++ client/components/MyPage/OrderSection/OrderSection.tsx
@@ -1,8 +1,9 @@
 import React, { ReactElement } from 'react';
 import * as S from './style';
 
-import Intro from '../Intro/Intro';
-import SearchBar from '../SearchBar/SearchBar';
+import Intro from '@components/MyPage/Intro/Intro';
+import SearchBar from '@components/MyPage/SearchBar/SearchBar';
+import ResultContainer from '@components/MyPage/ResultContainer/ResultContainer';
 
 interface Props {}
 
@@ -10,11 +11,48 @@ export default function OrderSection({}: Props): ReactElement {
   return (
     <S.OrderSection>
       <Intro {...sampleData} />
-      
       <SearchBar />
+      <ResultContainer title="주문목록 / 배송조회 내역 " results={results} />
     </S.OrderSection>
   );
 }
+
+interface IResult {
+  date: Date;
+  id: number;
+  orderNumber: string;
+  title: string;
+  option?: string;
+  productPrice: number;
+  productCount: number;
+  status: string; // 주문상태
+  checkOrReview: string; // 확인/리뷰
+}
+
+const results: IResult[] = [
+  {
+    date: new Date('2021-08-08'),
+    id: 4,
+    orderNumber: 'WOOTECHCAMP-DREAMD4-4',
+    title: 'ㅋㅋ슬리퍼',
+    option: '작은발',
+    productPrice: 15000,
+    productCount: 3,
+    status: '주문상태', // 주문상태
+    checkOrReview: '확인/리뷰', // 확인/리뷰
+  },
+
+  {
+    date: new Date('2021-08-09'),
+    id: 5,
+    orderNumber: 'WOOTECHCAMP-DREAMD4-5',
+    title: '맥주 휴대폰 홀더',
+    productPrice: 20000,
+    productCount: 1,
+    status: '주문상태', // 주문상태
+    checkOrReview: '확인/리뷰', // 확인/리뷰
+  },
+];
 
 const sampleData = {
   couponCount: 1,
