3f36ae02398f98f38f59bf78f140fed2482b1573
0woodev <tc_yeongu.n@woowafriends.com>
2021-08-13 05:59:07 +0900

    [#20] 마이페이지 내부 페이지 URL 변경

--- client/components/MyPage/NavigationBar/NavigationBar.tsx
+++ client/components/MyPage/NavigationBar/NavigationBar.tsx
@@ -1,33 +1,47 @@
 import { Link } from '@client/utils/router';
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState } from 'react';
 import * as S from './style';
 
-interface Props {}
+interface Props {
+  page: string;
+}
 
-export default function NavigationBar({}: Props): ReactElement {
+export default function NavigationBar({page}: Props): ReactElement {
+
+  const [selectedLink, setSelectedLink] = useState(0);
+
+  const handleClickButton = (e: React.MouseEvent) => {
+    const { target } = e;
+    if (!(target instanceof Link)) return;
+
+    setSelectedLink(+target);
+  };
+
+  const checkCurrentPage = (pageConstant: string): string => page === pageConstant ? 'clicked' : '';
+
   return (
   
   <S.NavigationBar>
     <h3>마이페이지</h3>
     
-    <S.NavigatorGroup>
+    <S.NavigatorGroup onClick={handleClickButton}>
       <h4>쇼핑정보</h4>
-      <Link to="/order-list">주문목록/배송조회</Link>
-      <Link to="/cancel-list">취소/반품/교환 내역</Link>
-      <Link to="/refund-list">환불 내역</Link>
-      <Link to="/wish-list">찜리스트</Link>
-      <Link to="/coupon">쿠폰함</Link>
-      <Link to="/mileage">적립금</Link>
+      <Link to="/mypage-order-list" className={checkCurrentPage('ORDER')}>주문목록/배송조회</Link>
+      <Link to="/mypage-cancel-list" className={checkCurrentPage('CANCEL')}>취소/반품/교환 내역</Link>
+      <Link to="/mypage-refund-list" className={checkCurrentPage('REFUND')}>환불 내역</Link>
+      <Link to="/mypage-wish-list" className={checkCurrentPage('WISH')}>찜리스트</Link>
+      <Link to="/mypage-coupon" className={checkCurrentPage('COUPON')}>쿠폰함</Link>
+      <Link to="/mypage-mileage" className={checkCurrentPage('MILEAGE')}>적립금</Link>
     </S.NavigatorGroup>
     
     
     <S.NavigatorGroup>
       <h4>회원정보</h4>
-      <Link to="/my-page-qa">1:1 문의게시판</Link>
-      <Link to="/my-page-password-check">회원정보 변경</Link>
-      <Link to="/shipping">배송지 관리</Link>
-      <Link to="/my-page-goods-qa">나의 상품문의</Link>
-      <Link to="/my-page-goods-review">나의 상품후기</Link>
+      <Link to="/mypage-qa" className={checkCurrentPage('QA')}>1:1 문의게시판</Link>
+      <Link to="/mypage-password-check" className={checkCurrentPage('PASSWORD_CHECK')}>회원정보 변경</Link>
+      <Link to="/mypage-address" className={checkCurrentPage('ADDRESS')}>배송지 관리</Link>
+      <Link to="/mypage-goods-qa" className={checkCurrentPage('GOODS_QA')}>나의 상품문의</Link>
+      <Link to="/mypage-goods-review" className={checkCurrentPage('GOODS_REVIEW')}>나의 상품후기</Link>
     </S.NavigatorGroup>
   </S.NavigationBar>
   );
