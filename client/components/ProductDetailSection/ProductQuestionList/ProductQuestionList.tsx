da00c80cf68ac57801bd076a4dbbaa3e98c4b7f0
hayoung123 <proshy1@naver.com>
2021-08-16 21:55:18 +0900

    [#47] feat: 문의 요청 폼 컴포넌트

--- client/components/ProductDetailSection/ProductQuestionList/ProductQuestionList.tsx
+++ client/components/ProductDetailSection/ProductQuestionList/ProductQuestionList.tsx
@@ -1,45 +1,58 @@
-import { ReactElement } from 'react';
+import { ReactElement, useState } from 'react';
 import * as S from './style';
 import * as CommonS from '../style';
 import QuestionItem from './QuestionItem/QuestionItem';
+import QuestionForm from './QuestionForm/QuestionForm';
 
 interface Props {}
 
 export default function ProductQuestionList({}: Props): ReactElement {
-  const questionList = sampleData
-    .map((data, idx) => <QuestionItem key={data.id} idx={idx} {...data} />)
-    .reverse();
+  const [isOpenForm, setIsOpenForm] = useState(false);
+  const questionList = sampleData.map((data) => <QuestionItem key={data.id} {...data} />);
+
+  const handlePostBtnClick = () => setIsOpenForm(true);
 
   return (
-    <S.ProductQuestionList>
-      <CommonS.UserFeedbackTitleContainer>
-        <div className="title">상품문의</div>
-        <button className="create-review-btn">상품문의 글쓰기</button>
-      </CommonS.UserFeedbackTitleContainer>
-      {/* <ul>${questionList.length ? { questionList } : <li>등록된 상품문의가 없습니다.</li>}</ul> */}
-      <CommonS.UserPostingList>
-        <li className="empty-msg">등록된 상품문의가 없습니다.</li>
-        {questionList}
-      </CommonS.UserPostingList>
-    </S.ProductQuestionList>
+    <>
+      <S.ProductQuestionList>
+        <CommonS.UserFeedbackTitleContainer>
+          <div className="title">상품문의</div>
+          <button className="create-review-btn" onClick={handlePostBtnClick}>
+            상품문의 글쓰기
+          </button>
+        </CommonS.UserFeedbackTitleContainer>
+        <CommonS.UserPostingList>
+          {questionList.length ? (
+            questionList
+          ) : (
+            <li className="empty-msg">등록된 상품문의가 없습니다.</li>
+          )}
+        </CommonS.UserPostingList>
+      </S.ProductQuestionList>
+      {isOpenForm && <QuestionForm setIsOpenForm={setIsOpenForm} />}
+    </>
   );
 }
 
 const sampleData = [
   {
-    id: '123',
+    id: '2',
     title: '색상은?',
+    type: '상품',
+    contents: '색상은 뭐가 어떻게 될까요?',
     userId: 'sample',
     isSecret: false,
     date: '2021.08.10',
     status: 'process',
   },
   {
-    id: '1243',
+    id: '1',
     title: '구매문의',
+    contents: '테스트 컨텐트 입니다',
     userId: 'aaample',
     isSecret: true,
     date: '2021.06.10',
     status: 'done',
+    answer: '테스트 답변완료',
   },
 ];
