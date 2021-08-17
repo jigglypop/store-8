bf8e480ebecfea0f0d172e22bd2268ebd826cb03
hayoung123 <proshy1@naver.com>
2021-08-18 01:19:06 +0900

    [#47] feat: 상품 문의 모델링
    - descriptio 을 content로 변경

--- server/models/Product-Question.ts
+++ server/models/Product-Question.ts
@@ -1,14 +1,16 @@
 import {
-  PrimaryKey,
-  AutoIncrement,
   Column,
   Model,
   Table,
   DataType,
   ForeignKey,
+  AllowNull,
+  BelongsTo,
 } from 'sequelize-typescript';
 
 import User from './User';
+import Product from './Product';
+import Product_Question_Reply from './Product-Question-Reply';
 
 interface IProductQuestion {
   [key: string]: any;
@@ -16,28 +18,42 @@ interface IProductQuestion {
 
 @Table
 export default class Product_Question extends Model<IProductQuestion> {
-  @PrimaryKey
-  @AutoIncrement
-  @Column(DataType.BIGINT)
+  @Column({ primaryKey: true })
   id: number;
 
+  @Column(DataType.STRING(60))
+  @AllowNull(false)
+  title: string;
+
+  @Column(DataType.STRING(500))
+  @AllowNull(false)
+  contents: string;
+
+  @Column(DataType.BOOLEAN)
+  @AllowNull(false)
+  isSecret: boolean;
+
   @Column
+  @AllowNull(false)
   @ForeignKey(() => User)
   userId: number;
 
-  //Product 추가
-  // @Column
-  // @ForeignKey(() => Product)
-  // productId: number;
-
-  @Column(DataType.STRING(60))
-  title: string;
+  @BelongsTo(() => User)
+  user: User;
 
   @Column
-  @Column(DataType.STRING(500))
-  description: string;
+  @AllowNull(false)
+  @ForeignKey(() => Product)
+  productId: number;
+
+  @BelongsTo(() => Product)
+  product: Product;
 
   @Column
-  @Column(DataType.BOOLEAN)
-  isSecret: boolean;
+  @AllowNull(false)
+  @ForeignKey(() => Product_Question_Reply)
+  replyId: number;
+
+  @BelongsTo(() => Product_Question_Reply)
+  reply: Product_Question_Reply;
 }
