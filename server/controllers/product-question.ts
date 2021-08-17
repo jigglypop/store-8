364470ea6b3061bd31e1b662cfc5dc66056df846
hayoung123 <proshy1@naver.com>
2021-08-17 16:31:33 +0900

    [#47] wip: 상품문의 모델링 작업 진행 중

---
+++ server/controllers/product-question.ts
@@ -0,0 +1,4 @@
+import { Request, Response } from 'express';
+
+export const create = async (req: Request, res: Response) => {};
+export const get = async (req: Request, res: Response) => {};
--- server/router/index.ts
+++ server/router/index.ts
@@ -1,12 +1,15 @@
-import express from "express";
-import userRouter from "./auth";
-import githubRouter from "./github";
-import templateRouter from "./template";
+import express from 'express';
+import userRouter from './auth';
+import ProductQuestionRouter from './product-question';
+import githubRouter from './github';
+import templateRouter from './template';
 
 const rootRouter = express();
 
-rootRouter.use("/api/auth", userRouter);
-rootRouter.use("/", templateRouter);
-rootRouter.use("/callback", githubRouter);
+rootRouter.use('/api/auth', userRouter);
+//TODO: 이름정하기
+rootRouter.use('/api/product-question', ProductQuestionRouter);
+rootRouter.use('/', templateRouter);
+rootRouter.use('/callback', githubRouter);
 
 export default rootRouter;
