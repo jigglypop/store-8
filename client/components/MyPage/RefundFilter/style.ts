c7053fd23cb72c3a86f65fd36ab7b2de3e5b5272
0woodev <tc_yeongu.n@woowafriends.com>
2021-08-19 01:23:42 +0900

    [#61] feat 반품/환불 필터 마크업 제작
    - [ ] 상태관리를 신경써서 추가해야 할것 같습니다.

---
+++ client/components/MyPage/RefundFilter/style.ts
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+
+export const RefundFilter = styled.div`
+  display: flex;
+  flex-direction: row;
+  height: 50px;
+  align-items: flex-end;
+  width: 100%;
+  border-bottom: 1px solid var(--gray3);
+  margin-top: 15px;
+`;
+
+interface IButtonIndex {
+  isClicked: boolean;
+}
+
+export const FilterButton = styled.button<IButtonIndex>`
+  background-color: white;
+  ${({ isClicked }) =>
+    isClicked
+      ? `
+    height: 65px; 
+    width: 200px;
+    border: 1px solid var(--gray3);
+    margin: -1px;    
+    z-index: 1;
+    border-bottom: none;
+    `
+      : `
+    height: 50px; 
+    width: 200px;
+    border: 1px solid var(--gray4);
+    border-bottom: none;
+    `}
+  align-items: flex-end;
+`;
--- client/components/MyPage/RefundSection/RefundSection.tsx
+++ client/components/MyPage/RefundSection/RefundSection.tsx
@@ -1,20 +1,112 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState } from 'react';
 import * as S from './style';
 
 import Intro from '../Intro/Intro';
 import SearchBar from '../SearchBar/SearchBar';
+import RefundFilter from '../RefundFilter/RefundFilter';
+import ResultContainer from '../ResultContainer/ResultContainer';
 
 interface Props {}
 
 export default function RefundSection({}: Props): ReactElement {
+  const [clickedBtnIdx, setClickedBtnIdx] = useState(0);
+
+  const setShownResult = () => {};
+
   return (
     <S.RefundSection>
       <Intro {...sampleData} />
-      <h2>RefundSection</h2>
-      <SearchBar />
+      <SearchBar title="반품/환불" />
+      <RefundFilter setShownResult={setShownResult} />
+      <ResultContainer title="반품/환불 내역 총 " results={results} />
     </S.RefundSection>
   );
 }
+
+interface IResult {
+  date: Date;
+  id: number;
+  orderNumber: string;
+  title: string;
+  option?: string;
+  productPrice: number;
+  productCount: number;
+  status: string; // 주문상태
+  checkOrReview: string; // 확인/리뷰
+  thumbnailSrc: string;
+}
+
+const results: IResult[] = [
+  {
+    date: new Date('2021-08-08'),
+    id: 4,
+    orderNumber: 'WTC-DREAMD4-4',
+    title: 'ㅋㅋ슬리퍼',
+    option: '작은발',
+    productPrice: 15000,
+    productCount: 3,
+    status: '주문상태', // 주문상태
+    checkOrReview: '확인/리뷰', // 확인/리뷰
+    thumbnailSrc: 'https://picsum.photos/40/50',
+  },
+  {
+    date: new Date('2021-08-09'),
+    id: 5,
+    orderNumber: 'WTC-DREAMD4-5',
+    title: '맥주 휴대폰 홀더',
+    productPrice: 20000,
+    productCount: 1,
+    status: '주문상태', // 주문상태
+    checkOrReview: '확인/리뷰', // 확인/리뷰
+    thumbnailSrc: 'https://picsum.photos/40/50',
+  },
+  {
+    date: new Date('2021-08-08'),
+    id: 4,
+    orderNumber: 'WTC-DREAMD4-4',
+    title: 'ㅋㅋ슬리퍼',
+    option: '작은발',
+    productPrice: 15000,
+    productCount: 3,
+    status: '주문상태', // 주문상태
+    checkOrReview: '확인/리뷰', // 확인/리뷰
+    thumbnailSrc: 'https://picsum.photos/40/50',
+  },
+  {
+    date: new Date('2021-08-09'),
+    id: 5,
+    orderNumber: 'WTC-DREAMD4-5',
+    title: '맥주 휴대폰 홀더',
+    productPrice: 20000,
+    productCount: 1,
+    status: '주문상태', // 주문상태
+    checkOrReview: '확인/리뷰', // 확인/리뷰
+    thumbnailSrc: 'https://picsum.photos/40/50',
+  },
+  {
+    date: new Date('2021-08-08'),
+    id: 4,
+    orderNumber: 'WTC-DREAMD4-4',
+    title: 'ㅋㅋ슬리퍼',
+    option: '작은발',
+    productPrice: 15000,
+    productCount: 3,
+    status: '주문상태', // 주문상태
+    checkOrReview: '확인/리뷰', // 확인/리뷰
+    thumbnailSrc: 'https://picsum.photos/40/50',
+  },
+  {
+    date: new Date('2021-08-09'),
+    id: 5,
+    orderNumber: 'WTC-DREAMD4-5',
+    title: '맥주 휴대폰 홀더',
+    productPrice: 20000,
+    productCount: 1,
+    status: '주문상태', // 주문상태
+    checkOrReview: '확인/리뷰', // 확인/리뷰
+    thumbnailSrc: 'https://picsum.photos/40/50',
+  },
+];
 
 const sampleData = {
   couponCount: 1,
