031a7690d169d793ba0c1a3aded9d34ab6bf2690
0woodev <tc_yeongu.n@woowafriends.com>
2021-08-20 07:46:36 +0900

    [#61] refactor: 날짜 쿼리 제작함수 수정

--- server/utils/make-query.ts
+++ server/utils/make-query.ts
@@ -1,80 +1,80 @@
-import { isEmptyObj, isEmptyToken } from "./result-checker";
-import { Op } from "sequelize";
+import { isEmptyObj, isEmptyToken } from './result-checker';
+import { Op } from 'sequelize';
 
-const makeWhereQueryWithDate = (startDate: string, endDate: Date) => {
-    const where = {
-        time: {}
+const makeWhereQueryWithDate = (key: string, startDate?: string, endDate?: string) => {
+  const where = {
+    [key]: {},
+  };
+
+  if (startDate && endDate) {
+    const _endDate = new Date(endDate);
+    _endDate.setDate(_endDate.getDate() + 1);
+
+    where[key] = {
+      [Op.between]: [new Date(startDate), new Date(endDate)],
     };
+  } else if (startDate) {
+    where[key] = {
+      [Op.gte]: new Date(startDate),
+    };
+  } else if (endDate) {
+    const _endDate = new Date(endDate);
+    _endDate.setDate(_endDate.getDate() + 1);
 
-    if (startDate && endDate) {
-        endDate = new Date(endDate);
-        endDate.setDate(endDate.getDate() + 1);
-    
-        where.time = {
-            [Op.between]: [new Date(startDate), new Date(endDate)],
-        };
-    } else if (startDate) {
-        where.time = {
-            [Op.gte]: new Date(startDate),
-        };
-    } else if (endDate) {
-        endDate = new Date(endDate);
-        endDate.setDate(endDate.getDate() + 1);
-        
-        where.time = {
-            [Op.lte]: new Date(endDate),
-        };
-    }
-    return where;
-}
+    where[key] = {
+      [Op.lte]: new Date(endDate),
+    };
+  }
+  return where;
+};
 
 const makeWhereQueryWithObj = (obj: any) => {
-    if (isEmptyObj(obj)) {
-        return {};
-    } 
+  if (isEmptyObj(obj)) {
+    return {};
+  }
 
-    Object.keys(obj).forEach(key => {
-        if (isEmptyToken(obj[key])) delete obj[key];
-    });
+  Object.keys(obj).forEach((key) => {
+    if (isEmptyToken(obj[key])) delete obj[key];
+  });
 
-    return obj;
-}
+  return obj;
+};
 
 const makeWhereQueryWithStringContain = (str: string) => {
-    const where = {
-        content: {}
+  const where = {
+    content: {},
+  };
+  if (str) {
+    where.content = {
+      [Op.like]: `%${str}%`,
     };
-    if (str) {
-        where.content = {
-            [Op.like]: `%${str}%`
-        };
-    }
-    return where;
-}
+  }
+  return where;
+};
 
 const makeWhereQueryWithRange = (minimum: number, maximum: number) => {
-    const where = {
-        value: {},
-    };
+  const where = {
+    value: {},
+  };
 
-    if (minimum && maximum) {
-        where.value = {
-            [Op.between]: [minimum, maximum],
-        };
-    } else if (minimum) {
-        where.value = {
-            [Op.gte]: minimum,
-        }
-    } else if (maximum) {
-        where.value = {
-            [Op.lte]: maximum,
-        }
-    }
-    return where;
-}
-export default {
-    makeWhereQueryWithDate,
-    makeWhereQueryWithObj,
-    makeWhereQueryWithStringContain,
-    makeWhereQueryWithRange,
+  if (minimum && maximum) {
+    where.value = {
+      [Op.between]: [minimum, maximum],
+    };
+  } else if (minimum) {
+    where.value = {
+      [Op.gte]: minimum,
+    };
+  } else if (maximum) {
+    where.value = {
+      [Op.lte]: maximum,
+    };
+  }
+  return where;
+};
+export {
+  makeWhereQueryWithDate,
+  makeWhereQueryWithObj,
+  makeWhereQueryWithStringContain,
+  makeWhereQueryWithRange,
 };
