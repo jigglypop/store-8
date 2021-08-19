f35a8f4d3461345113f37b044c3e5c3b37cba1b8
hayoung123 <proshy1@naver.com>
2021-08-20 02:03:12 +0900

    [#72] fix: createExtraGet 타입 에러로 인해 타입 변경

--- client/store/createExtra/createExtraGet.ts
+++ client/store/createExtra/createExtraGet.ts
@@ -1,28 +1,28 @@
-import { AsyncThunk } from "@reduxjs/toolkit";
+import { AsyncThunk } from '@reduxjs/toolkit';
 
 interface IState<TData> {
-    [props: string]: TData | string | boolean;
+  [props: string]: TData | string | boolean | number;
 }
 
 interface IAction<TData> {
-    payload: TData;
-    error: string;
+  payload: TData;
+  error: string;
 }
 
-const createExtraGet = <TData>(func: AsyncThunk<string, string, {}>, name: string) =>{
-    return {
-        [func.pending.type]: (state: IState<TData>) => {
-            state.loading = true;
-        },
-        [func.fulfilled.type]: (state: IState<TData>, action: IAction<TData>) => {
-            state.loading = false;
-            state[name] = action.payload;
-            state.error = ""
-        },
-        [func.rejected.type]: (state: IState<TData>, action: IAction<TData>) => {
-            state.loading = false;
-            state.error = action.payload
-        }
-    }
-}
-export default createExtraGet
+const createExtraGet = <TData>(func: AsyncThunk<string, any, {}>, name: string) => {
+  return {
+    [func.pending.type]: (state: IState<TData>) => {
+      state.loading = true;
+    },
+    [func.fulfilled.type]: (state: IState<TData>, action: IAction<TData>) => {
+      state.loading = false;
+      state[name] = action.payload;
+      state.error = '';
+    },
+    [func.rejected.type]: (state: IState<TData>, action: IAction<TData>) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+  };
+};
+export default createExtraGet;
