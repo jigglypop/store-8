c20abb1706ebd9b986813387523d59810e2af75d
hayoung123 <proshy1@naver.com>
2021-08-20 03:03:40 +0900

    [#74] feat: question 상태 이름 변경 및 커스텀 훅 생성

--- client/store/product/question.ts
+++ client/store/product/question.ts
@@ -4,20 +4,20 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import createExtraGet from '../createExtra/createExtraGet';
 
 interface IQuestion {
-  question: IQuestionRes | null;
+  question: IQuestionRes[] | null;
   error: string | null;
   loading: boolean;
 }
 
 const name = 'question';
 
-const QUESTION_GET = `${name}/get`;
-const QUESTION_CREATE = `${name}/post`;
-const QUESTION_UPDATE = `${name}/put`;
-const QUESTION_DELETE = `${name}/delete`;
+// const QUESTION_GET = `${name}/get`;
+// const QUESTION_CREATE = `${name}/post`;
+// const QUESTION_UPDATE = `${name}/put`;
+// const QUESTION_DELETE = `${name}/delete`;
 
-export const getQuestion = createAsyncThunk(QUESTION_GET, getQuestionApi);
-const questionInfoExtra = createExtraGet<IQuestionRes | null>(getQuestion, QUESTION_GET);
+export const getQuestion = createAsyncThunk(name, getQuestionApi);
+const questionInfoExtra = createExtraGet<IQuestionRes[] | null>(getQuestion, name);
 
 const initialState: IQuestion = {
   question: null,
