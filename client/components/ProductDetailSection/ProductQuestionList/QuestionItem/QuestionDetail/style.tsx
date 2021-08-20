fb4d6ce47699cbd807413696f64e279757cd3050
hayoung123 <proshy1@naver.com>
2021-08-21 02:23:39 +0900

    [#74] style: 상품문의 디테일 스타일 수정

--- client/components/ProductDetailSection/ProductQuestionList/QuestionItem/QuestionDetail/style.tsx
+++ client/components/ProductDetailSection/ProductQuestionList/QuestionItem/QuestionDetail/style.tsx
@@ -1,27 +1,47 @@
 import styled from 'styled-components';
 
 export const QuestionDetail = styled.div`
-  background-color: var(--background-gray);
-
-  & > div {
+  background-color: var(--footer-background-dark);
+  padding: 0 13px;
+  color: var(--text-black-dark);
+  .question-detail__question,
+  .question-detail__answer {
     display: flex;
     padding: 5px 0;
+    width: 100%;
+    min-height: 50px;
+  }
 
-    .question-detail__title,
-    .question-detail__content {
-      padding: 18px 13px;
+  .question-detail__content {
+    padding: 20px;
+    padding-right: 70px;
+    letter-spacing: 1px;
+    line-height: 1.5;
+    flex: 1;
+  }
+
+  .question-detail__title {
+    width: 60px;
+    padding-top: 25px;
+  }
+
+  .question-detail__date {
+    margin-top: 20px;
+    color: var(--text-gray);
+  }
+
+  .question-detail__btns {
+    display: flex;
+    align-items: center;
+    padding-right: 30px;
+    .question-detail__edit-btn {
+      cursor: pointer;
+      stroke: var(--text-black-dark);
     }
-
-    .question-detail__content {
-      padding-left: 10px;
-    }
-
-    .question-detail__title {
-      width: 60px;
-    }
-
-    .question-detail__date {
-      color: var(--text-gray);
+    .question-detail__delete-btn {
+      cursor: pointer;
+      stroke: var(--text-red);
+      margin-left: 20px;
     }
   }
 `;
