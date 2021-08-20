2d7f5c90401cdecb1670f03c223679655842c618
hayoung123 <proshy1@naver.com>
2021-08-20 23:22:18 +0900

    feat: fetch 전반적으로 사용할 수 있는 fetchWrapper

---
+++ client/utils/fetchWrapper.ts
@@ -0,0 +1,51 @@
+import cache from './cache';
+
+type MethodType = 'GET' | 'POST' | 'PUT' | 'DELETE' | 'HEAD';
+
+interface IResponse<T> {
+  success: true;
+  response: T;
+}
+interface IErrorResponse {
+  success: false;
+  errorMessage: string;
+}
+
+async function fetchWrapper<reqType, resType>(
+  url: string,
+  method: MethodType,
+  body?: reqType
+): Promise<IResponse<resType> | IErrorResponse> {
+  try {
+    const token = cache.get('token') ?? '';
+
+    const res = await fetch(url, {
+      method,
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      credentials: 'include', //refresh 토큰 때 cookie사용하기 위해서 미리 설정
+      body: JSON.stringify(body),
+    });
+
+    if (!res.ok) {
+      if (res.status === 401) {
+        //로그인 관련 에러
+        cache.remove('token');
+      }
+
+      const { message } = await res.json();
+      return { success: false, errorMessage: message };
+    }
+
+    const response = await res.json();
+
+    return { success: true, response };
+  } catch (err) {
+    console.log(err);
+    return { success: false, errorMessage: err.message };
+  }
+}
+
+export default fetchWrapper;
