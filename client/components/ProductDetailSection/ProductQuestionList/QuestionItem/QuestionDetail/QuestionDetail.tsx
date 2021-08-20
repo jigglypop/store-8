04cc78a217ef6a18f66e23d2b70e735daeacbc2b
hayoung123 <proshy1@naver.com>
2021-08-21 02:24:18 +0900

    [#74] feat: 상품문의 delete,edit 상태 등록

--- client/components/ProductDetailSection/ProductQuestionList/QuestionItem/QuestionDetail/QuestionDetail.tsx
+++ client/components/ProductDetailSection/ProductQuestionList/QuestionItem/QuestionDetail/QuestionDetail.tsx
@@ -1,5 +1,8 @@
-import { ReactElement } from 'react';
+import { ReactElement, useState } from 'react';
 import * as S from './style';
+import EditIcon from '@image/icon/editIcon.svg';
+import DeleteIcon from '@image/icon/deleteIcon.svg';
+import DeleteModal from '@components/common/DeleteModal/DeleteModal';
 
 interface Props {
   contents: string;
@@ -8,18 +11,38 @@ interface Props {
 }
 
 export default function QuestionDetail({ contents, answer, answerDate }: Props): ReactElement {
+  const [isEdit, setIsEdit] = useState(false);
+  const [isDelete, setIsDelete] = useState(false);
+
+  const cancelBtnCb = (isEdit: boolean) => () => {
+    isEdit ? setIsEdit(false) : setIsDelete(false);
+  };
+
+  const handleEditClick = () => setIsEdit(true);
+
+  const handleDeleteClick = () => setIsDelete(true);
+
   return (
     <S.QuestionDetail>
       <div className="question-detail__question">
         <div className="question-detail__title">Q</div>
         <div className="question-detail__content">{contents}</div>
+        <div className="question-detail__btns">
+          <EditIcon onClick={handleEditClick} className="question-detail__edit-btn" />
+          <DeleteIcon onClick={handleDeleteClick} className="question-detail__delete-btn" />
+        </div>
       </div>
       {answer && (
         <div className="question-detail__answer">
           <div className="question-detail__title">A</div>
-          <div className="question-detail__content">{answer}</div>
-          <div className="question-detail__date">{answerDate}</div>
+          <div className="question-detail__content">
+            {answer}
+            <div className="question-detail__date">{answerDate}</div>
+          </div>
         </div>
+      )}
+      {isDelete && (
+        <DeleteModal cancelCbFn={cancelBtnCb(false)} deleteCbFn={() => console.log('hello')} />
       )}
     </S.QuestionDetail>
   );
