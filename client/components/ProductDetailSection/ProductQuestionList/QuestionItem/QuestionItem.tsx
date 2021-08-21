3dbda33c820edb53c8b5417080c3ccc0df38b0f5
hayoung123 <proshy1@naver.com>
2021-08-21 12:52:53 +0900

    [#74] fix: 상품 문의 디테일 default로 꺼져있는 상태로 설정

--- client/components/ProductDetailSection/ProductQuestionList/QuestionItem/QuestionItem.tsx
+++ client/components/ProductDetailSection/ProductQuestionList/QuestionItem/QuestionItem.tsx
@@ -21,7 +21,7 @@ export default function QuestionItem({
   answer,
   answerDate,
 }: Props): ReactElement {
-  const [isOpenDetail, setIsOpenDetail] = useState(true);
+  const [isOpenDetail, setIsOpenDetail] = useState(false);
 
   const hideId = (id: string): string => {
     return id.slice(0, 2) + new Array(id.slice(2).length).fill('*').join('');
