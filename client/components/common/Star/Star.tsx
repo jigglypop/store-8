3d3b6ed6ea4af34c318b7ee99c344bba6756b7d0
hayoung123 <proshy1@naver.com>
2021-08-22 02:17:01 +0900

    [#88] feat: 리뷰 폼 score 체크 기능 및 star 크기 수정

--- client/components/common/Star/Star.tsx
+++ client/components/common/Star/Star.tsx
@@ -4,17 +4,23 @@ import StarIcon from '@image/icon/starIcon.svg';
 
 interface Props {
   isSelect: boolean;
+  handleClick?: () => void;
 }
 
-export default function Star({ isSelect }: Props): ReactElement {
+export default function Star({ isSelect, handleClick }: Props): ReactElement {
   return (
-    <StyledStar>
+    <StyledStar onClick={handleClick}>
       <StarIcon className={isSelect ? 'selected' : 'not-selected'} />
     </StyledStar>
   );
 }
 
 const StyledStar = styled.div`
+  & > svg {
+    width: 20px;
+    height: 20px;
+  }
+
   .selected {
     fill: var(--tag-picker);
   }
