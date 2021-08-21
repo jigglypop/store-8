e867d380afe744e7bc9ea35ba03e7ebdcb3f4184
hayoung123 <proshy1@naver.com>
2021-08-21 22:09:08 +0900

    [#88] feat: 리뷰 제출 폼 토글

--- client/components/ProductDetailSection/ProductReviewList/ProductReviewList.tsx
+++ client/components/ProductDetailSection/ProductReviewList/ProductReviewList.tsx
@@ -2,24 +2,31 @@ import { ReactElement, useState } from 'react';
 import * as S from './style';
 import * as CommonS from '../style';
 import { useReview } from '@client/hooks/review/review';
+
+import ReviewForm from './ReviewForm/ReviewForm';
 import ReviewItem from './ReviewItem/ReviewItem';
 
 interface Props {}
 
 export default function ProductReviewList({}: Props): ReactElement {
   const { review, loading, error } = useReview();
-  const [isOpenForm, setIsOpenForm] = useState(false);
+  const [isOpenForm, setIsOpenForm] = useState(true);
 
   //TODO USERID 목데이터 사용 중 로그인 적용 시 수정 예정
   const reviewList = review.map((data, idx) => {
     return <ReviewItem key={data.id} reviewData={data} idx={idx} userId="testId" />;
   });
 
+  const openReviewForm = () => setIsOpenForm(true);
+  const closeReviewForm = () => setIsOpenForm(false);
+
   return (
     <S.ProductReviewList>
       <CommonS.UserFeedbackTitleContainer>
         <div className="title">상품후기</div>
-        <button className="create-review-btn">상품후기 글쓰기</button>
+        <button onClick={openReviewForm} className="create-review-btn">
+          상품후기 글쓰기
+        </button>
       </CommonS.UserFeedbackTitleContainer>
       <CommonS.UserPostingList>
         {reviewList.length ? (
@@ -28,6 +35,7 @@ export default function ProductReviewList({}: Props): ReactElement {
           <li className="empty-msg">등록된 상품문의가 없습니다.</li>
         )}
       </CommonS.UserPostingList>
+      {isOpenForm && <ReviewForm closeReviewForm={closeReviewForm} />}
     </S.ProductReviewList>
   );
 }
