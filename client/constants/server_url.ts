7fc5c35d7f4e0e1365b3061cf2182a2dcb530b46
sshrik <chj878194@naver.com>
2021-08-23 01:33:52 +0900

    [#97] fix: server url to dot env.

--- client/constants/server_url.ts
+++ client/constants/server_url.ts
@@ -1,4 +1,4 @@
-export const SERVER_URL = 'http://localhost:3000';
-export const API_URL = 'http://localhost:3000/api';
+export const SERVER_URL = process.env.SERVER_URL;
+export const API_URL = process.env.SERVER_URL + '/api';
 export const BASE_URL = '/api/auth';
 export const GITHUB_URL = `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&redirect_uri=${process.env.SERVER_URL}/callback`;
