3c22d2feb20842554c8a262c258211a01b0d4623
sshrik <chj878194@naver.com>
2021-08-22 22:48:19 +0900

    [#69] style: footer redesign.

--- client/components/Footer/style.tsx
+++ client/components/Footer/style.tsx
@@ -1,81 +1,62 @@
 import styled from '@lib/styledComponent';
 
-// 푸터 부분
 export const Footer = styled.footer`
   position: relative;
   bottom: 0;
   width: 100%;
-
-  .footerInner {
-    position: relative;
-    width: 100%;
-    height: 230px;
-    bottom: 0;
-    background: var(--footer-background-dark);
-  }
-  .footerWrap {
-    width: 1200px;
-    margin: 0 auto;
-    padding-top: 30px;
-    display: grid;
-    grid-template-columns: 1fr 1fr 4fr;
-  }
+  height: 240px;
+  background: var(--background-gray);
+  padding-left: 120px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
 
   .footerLogo {
-    grid-column: 1/2;
-    width: 300px;
-    display: flex;
-    justify-content: flex-end;
-    align-items: center;
-
-    img {
-      width: 100px;
-    }
+    transform: scale(0.4);
+    transform-origin: center;
+    margin-right: -120px;
   }
-  .footerTextTop {
-    grid-column: 2/3;
-    width: 100%;
-    height: 100%;
+
+  .footerTextContainer {
     display: flex;
     flex-direction: column;
-    align-items: center;
-    justify-content: center;
+    justify-content: space-between;
+    height: 114px;
+  }
 
-    div {
-      margin: 5px;
-      font-size: 14px;
-      color: var(--gray2);
-      font-weight: 800;
+  .footerTextTop {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    p {
+      font-size: var(--body-middle-font);
+      color: var(--text-dark-gray);
+      font-weight: 700;
+      margin-right: 12px;
     }
   }
 
   .footerTextBottom {
-    grid-column: 3/4;
-    margin-top: 20px;
-
     display: flex;
     flex-direction: column;
-    align-items: flex-start;
-    justify-content: flex-start;
-
-    .footer-text-bottom-item {
+    .footerRow {
       display: flex;
-      align-items: center;
-      justify-content: center;
-      margin: 3px;
-
-      div {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        margin: 3px;
-
-        p {
-          padding: 3px;
-          font-size: 12px;
-          color: var(--text-black-dark);
-          border-right: 2px solid var(--grayC);
-        }
+      flex-direction: row;
+      margin-top: 12px;
+      p {
+        font-size: var(--body-middle-font);
+      }
+      .footerRowTitle {
+        margin-right: 8px;
+      }
+      .footerCopyTitle {
+        margin-right: 8px;
+        font-weight: 600;
+      }
+      .footerEmail {
+        color: var(--text-mint);
+        text-decoration: underline;
+        cursor: pointer;
       }
     }
   }
