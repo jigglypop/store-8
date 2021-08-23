2c6a719fed0b3c90355488be15e7dd3e792aa47a
0woodev <tc_yeongu.n@woowafriends.com>
2021-08-24 03:41:32 +0900

    [#61] 불필요한 import 제거

--- client/hooks/myRefund/myRefund.ts
+++ client/hooks/myRefund/myRefund.ts
@@ -1,8 +1,6 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '@client/store';
-import { useEffect } from 'react';
 import { getMyRefund } from '@client/store/myRefund/myRefund';
-import { create } from 'ts-node';
 
 export function useMyRefund() {
   const { myRefund } = useSelector((state: RootState) => state.myRefund);
