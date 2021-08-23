885dbc955359170620ccc9191b78cb587b1c7a18
hayoung123 <proshy1@naver.com>
2021-08-23 19:00:19 +0900

    [#100] style: option 상품선택 마크업

--- client/components/ProductDetail/ProductInfo/ProductOption/ProductOption.tsx
+++ client/components/ProductDetail/ProductInfo/ProductOption/ProductOption.tsx
@@ -2,21 +2,42 @@ import React, { ReactElement, useState } from 'react';
 import * as S from './style';
 import { IOption } from '@middle/type/product/options';
 import ArrowDown from '@image/icon/arrowDownIcon.svg';
+import { useProduct } from '@client/hooks/product/product';
 
 interface Props {
   optionData: IOption[];
 }
 
 export default function ProductOption({ optionData }: Props): ReactElement {
+  const { product } = useProduct();
+  if (!product) return <></>;
+
   const [isOpen, setIsOpen] = useState(false);
+  const toggleOpen = () => setIsOpen((isOpen) => !isOpen);
+  const selectableOptionList = optionData.map((option) => {
+    return (
+      <li key={option.id} className="selectable-item">
+        [{product.title}] {option.title}
+      </li>
+    );
+  });
+
   return (
-    <S.ProductOption>
+    <S.ProductOption isOpen={isOpen}>
       <S.InfoTitle>상품 선택</S.InfoTitle>
-      <div className="option__box">
-        <div>상품선택</div>
-        <ArrowDown className="option__down-arrow" />
-      </div>
-      {isOpen && <ul></ul>}
+      <ul onClick={toggleOpen} className="option__box">
+        <li className="option__box-title">
+          <div>상품선택</div>
+          <ArrowDown className="option__down-arrow" />
+        </li>
+        {isOpen && (
+          <ul className="option__selectable-wrapper">
+            {selectableOptionList}
+            {selectableOptionList}
+            {selectableOptionList}
+          </ul>
+        )}
+      </ul>
     </S.ProductOption>
   );
 }
--- client/components/ProductDetail/ProductInfo/ProductOption/style.tsx
+++ client/components/ProductDetail/ProductInfo/ProductOption/style.tsx
@@ -1,24 +1,74 @@
 import styled from 'styled-components';
 
-export const ProductOption = styled.div`
+interface ProductOptionProps {
+  isOpen: boolean;
+}
+
+export const ProductOption = styled.div<ProductOptionProps>`
+  background-color: var(--body-background-dark);
   display: flex;
-  align-items: center;
   .option__box {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
+    background-color: var(--body-background-dark);
+    position: relative;
     width: 380px;
-    padding: 3px 9px 3px 24px;
+
     border: 1px solid var(--line-gray-dark);
-    & > div {
+
+    .option__box-title {
+      background-color: var(--body-background-dark);
+      cursor: pointer;
+      width: 100%;
       display: flex;
-      align-items: cen;
+      align-items: center;
+      justify-content: space-between;
       padding-top: 5px;
+      padding: 3px 9px 3px 16px;
+      border-bottom: ${({ isOpen }) => (isOpen ? '1px solid var(--line-gray-dark)' : 'none')};
+      .option__down-arrow {
+        width: 36px;
+        height: 36px;
+        fill: var(--tag-picker);
+        transform: ${({ isOpen }) => (isOpen ? 'rotate(180deg)' : '')};
+      }
     }
-    .option__down-arrow {
-      width: 36px;
-      height: 36px;
-      fill: var(--text-pastel-black-dark);
+
+    .option__selectable-wrapper {
+      position: absolute;
+      border: 1px solid var(--line-gray-dark);
+      border-top: none;
+      width: 380px;
+      left: -1px;
+      .selectable-item {
+        background-color: var(--background-option-select);
+        height: 44px;
+        display: flex;
+        align-items: center;
+        padding-left: 16px;
+        &:not(:last-child) {
+          border-bottom: 1px solid var(--line-gray-dark);
+        }
+      }
+      .selectable-item:hover {
+        background-color: var(--text-pastel-black-reverse-dark);
+      }
+    }
+  }
+
+  .option__select-item {
+    position: absolute;
+    top: 42px;
+    left: -1px;
+    width: 100%;
+
+    li {
+      display: flex;
+      align-items: center;
+      height: 40px;
+      padding-left: 16px;
+      border: 1px solid var(--line-gray-dark);
+      &:not(:last-child) {
+        border-bottom: none;
+      }
     }
   }
 `;
@@ -28,4 +78,5 @@ export const ProductOptionItem = styled.div``;
 export const InfoTitle = styled.div`
   width: 150px;
   color: var(--text-gray-dark);
+  padding-top: 14px;
 `;
