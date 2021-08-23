0f8c290122909d440768393667f5a5cb1736d806
0woodev <tc_yeongu.n@woowafriends.com>
2021-08-24 04:28:05 +0900

    [#61] 나의 정보수정 페이지 네비게이션에 추가, 각 페이지 중앙정렬

--- client/pages/MyInfoEditPage/style.ts
+++ client/pages/MyInfoEditPage/style.ts
@@ -1,11 +1,11 @@
 import styled from 'styled-components';
 
-export const MyOrderPage = styled.div`
+export const MyInfoEditPage = styled.div`
   margin-top: 12px;
   margin-left: 12px;
   display: flex;
   flex-direction: row;
-  justify-content: center;
   gap: 32px;
+  justify-content: center;
   min-height: 650px;
 `;
--- client/pages/MyRefundPage/style.ts
+++ client/pages/MyRefundPage/style.ts
@@ -5,6 +5,7 @@ export const MyRefundPage = styled.div`
   margin-left: 12px;
   display: flex;
   flex-direction: row;
+  justify-content: center;
   gap: 32px;
   min-height: 650px;
 `;
