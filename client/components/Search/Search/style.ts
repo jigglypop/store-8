a239b1f5278c5eccab3b3fa7bdd347f6df12399c
jigglypop <52653682+jigglypop@users.noreply.github.com>
2021-08-24 21:13:36 +0900

    [#105] 페이지부분, 검색시 제목변경

--- client/components/Search/Search/style.ts
+++ client/components/Search/Search/style.ts
@@ -1,86 +1,53 @@
+import { MainItem } from '@client/components/Main/common/style';
 import styled from 'styled-components';
 
-export const Search = styled.div`
+export const Search = styled(MainItem)`
   position: relative;
+  width: 100%;
   display: flex;
   justify-content: center;
-  align-items: flex-start;
-`;
+  align-items: center;
 
-export const SearchInner = styled.div`
-  position: sticky;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  align-items: flex-start;
-  text-align: center;
-  transition: all 0.3s ease-in-out;
-
-  height: 40px;
-  width: 100px;
-  border-radius: 20px;
-  background: rgba(0, 0, 0, 0.8);
-  box-shadow: 0 0 10px var(--black);
-  -webkit-backdrop-filter: blur(10px);
-  backdrop-filter: blur(10px);
-
-  .tags {
+  .content.isNone {
+    position: relative;
+    width: 100%;
+    min-width: 60vw;
+    min-height: 60vh;
     display: flex;
-    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
   }
-  .elastic {
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    align-items: flex-start;
-    text-align: flex-start;
 
-    .elastic-item {
+  .search-inner {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: rgb(0, 0, 0, 0.8);
+    padding: 30px 20px;
+    border-radius: 20px;
+    box-shadow: 0 0 20px rgb(0, 0, 0, 0.8);
+
+    h4 {
+      font-size: 16px;
       color: var(--text-white);
-      margin: 5px;
-      font-size: 14px;
-    }
-  }
-  input {
-    font-size: 16px;
-    outline: none;
-    padding: 20px;
-    height: 40px;
-    width: 100px;
-    box-sizing: border-box;
-    color: var(--text-white);
-    transition: all 0.3s ease-in-out;
-  }
-
-  &.wide {
-    transform: translateY(40%);
-    height: 300px;
-    width: 200px;
-    .elastic {
-      min-width: 100px;
-    }
-    input {
-      margin: 10px;
-      width: 180px;
-      border-bottom: 2px solid var(--gray3);
     }
 
-    .tag-item {
+    .search-items {
       display: flex;
+      flex-direction: row;
       justify-content: center;
       align-items: center;
-      height: 30px;
-      font-size: 12px;
-      margin: 5px;
-      padding: 5px;
-      background-color: var(--text-white);
-      border-radius: 10px;
-      color: var(--text-black);
+    }
 
-      span {
-        margin: 0 5px;
-        cursor: pointer;
-      }
+    .search-item {
+      margin: 10px;
+      font-size: 14px;
+      font-weight: 600;
+      color: var(--text-white);
+      cursor: pointer;
+    }
+    .search-item.isSelected {
+      color: var(--text-picker);
     }
   }
 `;
