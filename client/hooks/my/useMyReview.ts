b4c00bbc4624b473504fbe69f604bc8a13d73828
hayoung123 <proshy1@naver.com>
2021-08-24 21:36:40 +0900

    [#113] feat: myreview, myquestion 커스텀 훅

---
+++ client/hooks/my/useMyReview.ts
@@ -0,0 +1,87 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { RootState } from '@client/store';
+
+import { useRouter } from '../router/router';
+import { getReview, setError, setPage } from '@client/store/product/review';
+import {
+  createReviewApi,
+  updateReviewApi,
+  deleteReviewApi,
+  likeReviewApi,
+} from '@client/api/review';
+import {
+  IReviewPostReq,
+  IReviewPutReq,
+  IReviewDeleteReq,
+  IReviewLikeReq,
+} from '@middle/type/review/review';
+import cache from '@client/utils/cache';
+
+type IFetchType = 'create' | 'update' | 'delete';
+
+export function useReview() {
+  const {
+    router: { params },
+  } = useRouter();
+
+  const productId = +params;
+  const dispatch = useDispatch();
+
+  const { review, currentPage, error, loading } = useSelector((state: RootState) => state.review);
+
+  const fetchReview =
+    <T>(type: IFetchType) =>
+    async (reviewForm: T): Promise<boolean> => {
+      let res;
+
+      if (type === 'create') res = await createReviewApi<T>(productId, reviewForm);
+      if (type === 'update') res = await updateReviewApi<T>(productId, reviewForm);
+      if (type === 'delete') res = await deleteReviewApi<T>(productId, reviewForm);
+
+      if (!res) return false;
+
+      if (!res.success) {
+        dispatch(setError(res.errorMessage));
+        return false;
+      }
+      dispatch(getReview({ productId, query: `page=${currentPage}`, token: cache.get('token') }));
+      return true;
+    };
+
+  const likeReview = async (reviewId: number, reviewLikeForm: IReviewLikeReq): Promise<boolean> => {
+    const res = await likeReviewApi(reviewId, reviewLikeForm);
+    if (!res) return false;
+
+    if (!res.success) {
+      dispatch(setError(res.errorMessage));
+      return false;
+    }
+    dispatch(getReview({ productId, query: `page=${currentPage}`, token: cache.get('token') }));
+    return true;
+  };
+
+  const setCurrentPage = (newPage: number) => {
+    dispatch(setPage(newPage));
+    dispatch(getReview({ productId, query: `page=${newPage}`, token: cache.get('token') }));
+  };
+
+  // 페이지 시작
+  useEffect(() => {
+    dispatch(getReview({ productId, query: `page=${currentPage}`, token: cache.get('token') }));
+  }, []);
+
+  return {
+    totalCount: review?.totalCount ?? 0,
+    reviews: review?.reviews ?? [],
+    loading,
+    error,
+    currentPage,
+    setCurrentPage,
+    createReview: fetchReview<IReviewPostReq>('create'),
+    updateReview: fetchReview<IReviewPutReq>('update'),
+    deleteReview: fetchReview<IReviewDeleteReq>('delete'),
+    likeReview,
+  };
+}
