0e604fa8c20d18d5201e34116db0a5b0d7111955
sshrik <chj878194@naver.com>
2021-08-24 10:10:49 +0900

    [#104] fix: wish가 잘 안나타나던 현상 해결.

--- client/components/Wish/Wish.tsx
+++ client/components/Wish/Wish.tsx
@@ -1,6 +1,7 @@
 import * as S from './style';
 import Heart from '@image/svg/Heart.svg';
 import { useWish } from '@client/hooks/wish/wish';
+import cache from '@utils/cache';
 
 type IWish = {
   productId: string;
@@ -8,7 +9,8 @@ type IWish = {
 };
 
 export default function Wish({ productId, name }: IWish) {
-  const { isInMyWish, isLoggedIn, toggleWish } = useWish(productId, name);
+  const { isInMyWish, toggleWish } = useWish(productId, name);
+  const isLoggedIn = cache.get('token');
   return (
     <>
       {isLoggedIn && (
