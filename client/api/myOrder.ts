49946f938337a86476227470e31bc634c90ba1ed
0woodev <tc_yeongu.n@woowafriends.com>
2021-08-25 13:16:05 +0900

    [#127] feat: client API 제작 - 구매확정 API

--- client/api/myOrder.ts
+++ client/api/myOrder.ts
@@ -6,13 +6,24 @@ interface IDateReq {
   endDate: string;
 }
 
-// 찜 목록
 export const myOrderApi = async ({ startDate, endDate }: IDateReq, thunkApi: IThunkApi) => {
   const token = cache.get('token');
   const data = await request.getToken(
     `/api/order?startDate=${startDate}&endDate=${endDate}`,
     token
   );
+  if (data.status !== 200) {
+    const error = data.message;
+    return await thunkApi.rejectWithValue(error);
+  }
+
+  return data.data;
+};
+
+export const myOrderConfirmApi = async (orderId: Number, thunkApi: IThunkApi) => {
+  const token = cache.get('token');
+  const data = await request.post(`/api/order/confirm/${orderId}`, token);
+
   if (data.status !== 200) {
     const error = data.message;
     return await thunkApi.rejectWithValue(error);
