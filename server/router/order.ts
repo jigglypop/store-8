2084a6169f46ea7f31268971b7d510d29dd05d41
0woodev <tc_yeongu.n@woowafriends.com>
2021-08-26 16:12:18 +0900

    [#125] feat: Order 테이블 변경으로 인한 수정 및 새로운 API 연결
    - [ ] 구매확정 API

--- server/router/order.ts
+++ server/router/order.ts
@@ -2,12 +2,14 @@ import express, { Router } from 'express';
 
 // import jwtMiddleware from '../middleware/jwtMiddleware';
 import wrapAsync from '../utils/wrapAsync';
-import { getAllOrders, createOrder, getMileage } from '../controllers/order';
+import { getAllOrders, createOrder, getMileage, updateOrderState } from '../controllers/order';
 
 const orderRouter: Router = express.Router();
 
 orderRouter.get('/', wrapAsync(getAllOrders));
+orderRouter.get('/:productId', wrapAsync(getAllOrders));
 orderRouter.post('/mileage', wrapAsync(getMileage));
 orderRouter.post('/create', wrapAsync(createOrder));
+orderRouter.post('/confirm/:id', wrapAsync(updateOrderState));
 
 export default orderRouter;
