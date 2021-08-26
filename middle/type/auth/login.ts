0be18df187ce88f8d888d658b63df22789e15518
0woodev <tc_yeongu.n@woowafriends.com>
2021-08-26 16:16:28 +0900

    [#125] prettier

--- middle/type/auth/login.ts
+++ middle/type/auth/login.ts
@@ -1,13 +1,13 @@
 export interface ILoginReq {
-    username: string;
-    password: string;
-    [key: string]: string;
+  username: string;
+  password: string;
+  [key: string]: string;
 }
 
 export interface ILoginRes {
-    id: number;
-    username: string;
-    email: string;
-    updatedAt: Date;
-    createdAt: Date;
+  id: number;
+  username: string;
+  email: string;
+  updatedAt: Date;
+  createdAt: Date;
 }
