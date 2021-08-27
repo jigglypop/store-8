3487224d8a834492e1c800ae504ad671b4d54cee
hayoung123 <proshy1@naver.com>
2021-08-27 23:22:43 +0900

    [#167] feat: 반응형 커스텀훅 구현

---
+++ client/hooks/useResponse.tsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react';
+
+import { debounce } from '@client/utils/performance';
+
+interface IArgs {
+  minWidth?: number;
+  maxWidth?: number;
+}
+
+export const useResponse = ({ minWidth, maxWidth }: IArgs) => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const handleWindowSize = debounce(() => {
+    setWindowWidth(window.innerWidth);
+  }, 50);
+
+  useEffect(() => {
+    window.addEventListener('resize', handleWindowSize);
+    return () => window.removeEventListener('resize', handleWindowSize);
+  }, []);
+
+  if (minWidth && maxWidth) return windowWidth >= minWidth && windowWidth < maxWidth;
+  else if (maxWidth) return windowWidth < maxWidth;
+  else if (minWidth) return windowWidth >= minWidth;
+  return false;
+};
