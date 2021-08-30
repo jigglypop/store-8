64a00471f68090ad34313367fbbce7a844d48cb2
hayoung123 <proshy1@naver.com>
2021-08-30 10:46:11 +0900

    [#190] fix: 상세페이지 클린업

--- client/pages/ProductDetailPage/ProductDetailPage.tsx
+++ client/pages/ProductDetailPage/ProductDetailPage.tsx
@@ -1,9 +1,21 @@
+import { useEffect } from 'react';
 import * as S from './style';
+import { useDispatch } from 'react-redux';
 
 import ProductDetail from '@components/ProductDetail/ProductDetail';
 import ProductDetailSection from '@client/components/ProductDetailSection/ProductDetailSection';
 
+import { initProduct } from '@client/store/product/product';
+
 export default function ProductDetailPage() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    return () => {
+      dispatch(initProduct());
+    };
+  }, []);
+
   return (
     <S.ProductDetailPage>
       <ProductDetail />
